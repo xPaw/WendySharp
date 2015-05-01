3dca95541c567635247d0f05a105fd8bb04883b6
xPaw <github@xpaw.me>
2015-05-01 22:39:59 +0300

    Update op/deop/voice/devoice commands to send flags in a single message

--- Commands/Deop.cs
+++ Commands/Deop.cs
@@ -20,7 +20,7 @@ namespace WendySharp
 
         public override void OnCommand(CommandArguments command)
         {
-            var nicks = new List<string>();
+            var nicks = new List<IrcString>();
             var input = command.Arguments.Groups["nicks"].Value;
 
             if (input.Length == 0)
@@ -36,7 +36,7 @@ namespace WendySharp
                 {
                     if (!IrcIdentity.TryParse(nick, out ident))
                     {
-                        command.Reply("Invalid identity.");
+                        command.Reply("'{0}' is a invalid indentity.", nick);
 
                         return;
                     }
@@ -47,22 +47,19 @@ namespace WendySharp
 
             var channel = Bootstrap.Client.ChannelList.GetChannel(command.Event.Recipient);
 
-            foreach (var nick in nicks)
+            if (channel.WeAreOpped)
             {
-                if (channel.WeAreOpped)
-                {
-                    Bootstrap.Client.Client.Mode(command.Event.Recipient, "-o", new IrcString[1] { nick });
-                }
-                else if (channel.HasUser("ChanServ"))
-                {
-                    Bootstrap.Client.Client.Message("ChanServ", string.Format("deop {0} {1}", channel.Name, nick));
-                }
-                else
-                {
-                    command.Reply("I'm not opped, send help.");
+                Bootstrap.Client.Client.Mode(command.Event.Recipient, "-" + new string('o', nicks.Count), nicks.ToArray());
+            }
+            else if (channel.HasUser("ChanServ"))
+            {
+                Bootstrap.Client.Client.Message("ChanServ", string.Format("deop {0} {1}", channel.Name, string.Join(" ", nicks)));
+            }
+            else
+            {
+                command.Reply("I'm not opped, send help.");
 
-                    return;
-                }
+                return;
             }
 
             Log.WriteInfo("Deop", "{0} took channel operator from {1} in {2}", command.Event.Sender, string.Join(", ", nicks), command.Event.Recipient);
--- Commands/Devoice.cs
+++ Commands/Devoice.cs
@@ -22,7 +22,7 @@ namespace WendySharp
 
         public override void OnCommand(CommandArguments command)
         {
-            var nicks = new List<string>();
+            var nicks = new List<IrcString>();
             var input = command.Arguments.Groups["nicks"].Value;
 
             if (input.Length == 0)
@@ -38,7 +38,7 @@ namespace WendySharp
                 {
                     if (!IrcIdentity.TryParse(nick, out ident))
                     {
-                        command.Reply("Invalid identity.");
+                        command.Reply("'{0}' is a invalid indentity.", nick);
 
                         return;
                     }
@@ -49,22 +49,19 @@ namespace WendySharp
 
             var channel = Bootstrap.Client.ChannelList.GetChannel(command.Event.Recipient);
 
-            foreach (var nick in nicks)
+            if (channel.WeAreOpped)
             {
-                if (channel.WeAreOpped)
-                {
-                    Bootstrap.Client.Client.Mode(command.Event.Recipient, "-v", new IrcString[1] { nick });
-                }
-                else if (channel.HasUser("ChanServ"))
-                {
-                    Bootstrap.Client.Client.Message("ChanServ", string.Format("devoice {0} {1}", channel.Name, nick));
-                }
-                else
-                {
-                    command.Reply("I'm not opped, send help.");
+                Bootstrap.Client.Client.Mode(command.Event.Recipient, "-" + new string('o', nicks.Count), nicks.ToArray());
+            }
+            else if (channel.HasUser("ChanServ"))
+            {
+                Bootstrap.Client.Client.Message("ChanServ", string.Format("devoice {0} {1}", channel.Name, string.Join(" ", nicks)));
+            }
+            else
+            {
+                command.Reply("I'm not opped, send help.");
 
-                    return;
-                }
+                return;
             }
 
             Log.WriteInfo("Deop", "{0} took voice from {1} in {2}", command.Event.Sender, string.Join(", ", nicks), command.Event.Recipient);
