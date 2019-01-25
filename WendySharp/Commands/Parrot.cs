7276059d806ac5998686a61f8cc9e220c17aef9d
Pavel Djundik <xPaw@users.noreply.github.com>
2019-01-25 14:12:10 +0200

    Add max length to parrot

--- WendySharp/Commands/Parrot.cs
+++ WendySharp/Commands/Parrot.cs
@@ -20,6 +20,12 @@ namespace WendySharp
         {
             var text = command.Arguments.Groups["text"].Value;
 
+            if (text.Length > 140)
+            {
+                command.Reply("That message is too long to be parroted.");
+                return;
+            }
+
             Log.WriteInfo("Parrot", "'{0}' said to '{1}': {2}", command.Event.Sender, command.Event.Recipient, text);
 
             Bootstrap.Client.Client.Message(command.Event.Recipient, string.Format("\u200B{0}", text));
