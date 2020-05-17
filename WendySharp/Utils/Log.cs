d24bd7383f0425a370b8a4bafa0ec65442a2f9d4
Pavel Djundik <xPaw@users.noreply.github.com>
2020-05-17 12:50:07 +0300

    Check arg count before formatting in logs

--- WendySharp/Utils/Log.cs
+++ WendySharp/Utils/Log.cs
@@ -30,29 +30,14 @@ namespace WendySharp
             }
         }
 
-        public static void WriteDebug(string component, string format, params object[] args)
-        {
-            WriteLine(Category.DEBUG, component, format, args);
-        }
-
-        public static void WriteInfo(string component, string format, params object[] args)
-        {
-            WriteLine(Category.INFO, component, format, args);
-        }
-
-        public static void WriteWarn(string component, string format, params object[] args)
-        {
-            WriteLine(Category.WARN, component, format, args);
-        }
-
-        public static void WriteError(string component, string format, params object[] args)
-        {
-            WriteLine(Category.ERROR, component, format, args);
-        }
+        public static void WriteDebug(string component, string format, params object[] args) => WriteLine(Category.DEBUG, component, format, args);
+        public static void WriteInfo(string component, string format, params object[] args) => WriteLine(Category.INFO, component, format, args);
+        public static void WriteWarn(string component, string format, params object[] args) => WriteLine(Category.WARN, component, format, args);
+        public static void WriteError(string component, string format, params object[] args) => WriteLine(Category.ERROR, component, format, args);
 
         private static void WriteLine(Category category, string component, string format, params object[] args)
         {
-            var logLine = $"{DateTime.Now.ToString("s").Replace('T', ' ')} [{category}] {component}: {string.Format(format, args)}{Environment.NewLine}";
+            var logLine = $"{DateTime.Now.ToString("s").Replace('T', ' ')} [{category}] {component}: {(args.Length > 0 ? string.Format(format, args) : format)}{Environment.NewLine}";
 
             lock (logLock)
             {
@@ -65,6 +50,12 @@ namespace WendySharp
                 else if (category == Category.DEBUG)
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(logLine);
+                    Console.ResetColor();
+                }
+                else if (category == Category.WARN)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
                     Console.Write(logLine);
                     Console.ResetColor();
                 }
